ccc15243b34dd8f1443c561037747646552e08ff
CodePhantom01 <rohit047276@gmail.com>
2026-05-30 11:59:49 +0530

    Added offline music downloads and PWA support

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,17 +1,24 @@
-const CACHE_NAME = "dhoon-v1";
+const CACHE_NAME = "dhoon-v2";
+
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/logo192.png",
+  "/logo512.png"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(["/"]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
